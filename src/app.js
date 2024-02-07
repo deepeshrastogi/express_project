e1ea5f1c87444f79edc7d818e3bd17760413e023
deepeshrastogi <deepesh.rastogi@maracana.in>
2024-02-07 12:10:25 +0530

    remove unwanted code for static website

--- src/app.js
+++ src/app.js
@@ -5,32 +5,6 @@ const path = require("path");
 const staticPath = path.join(__dirname,'../public');
 // build in middleware
 app.use(express.static(staticPath));
-app.get('/',(req,res) => {
-    res.json([
-        {
-            id:1,
-            name:'Deepesh',
-            age:24
-        },
-        {
-            id:2,
-            name:'Rastogi',
-            age:28
-        },
-    ])
-});
-
-app.get('/about',(req,res) => {
-    res.status(200).send('<h2>hello friend this is about page</h2>');
-});
-
-app.get('/contact',(req,res) => {
-    res.write("<h2>hello friend this is contact page</h2>");
-    res.write("<h2>hello friend this is again contact page</h2>");
-    res.send();
-});
-
-
 app.listen(port,()=>{
     console.log(`app is runninig on ${port} port`);
 });
